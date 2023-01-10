0dae196dce4b1469ac99ef23cd0cd09db82ebb06
Mathilde Aguiar <mathilde.aguiar@etu.univ-grenoble-alpes.fr>
2023-01-10 20:31:57 +0100

    script to parse the csv output of the measures

---
+++ src/app/visu_utils.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import glob
+import re
+
+def map_time_slice(time_range):
+    """
+    Get the time slice used to compute the score and return the mean year to choose the position of the cloud on the graph.
+    """
+    start, end = time_range.split("-")
+    mean_year = str(round((int(start)+int(end))/2))
+    return mean_year
+    
+
+def read_results_one_period(res_file):
+    """
+    :str: res_file : CSV output of measure_semantic_shift.py
+
+    Read the CSV file from the measure_semantic_shift.py and wrap it into an nice formatted dataframe for the visualization.
+    """
+    # TODO rm
+    fill_color = { "gold": "#efcf6d", "silver": "#cccccc", "bronze": "#c59e8a" }
+
+
+    #columns = ['Word', 'Score']
+    df = pd.read_csv(res_file)  #, names=columns
+    nb_words = df['word'].size
+    
+    # Find the time slice and change it into a single year that match our predefined axis
+    time_slice = re.findall("\d+-\d+",df.columns[1])[0]  #'1910-1950'
+    year = map_time_slice(time_slice)  # TODO parse this info in the file name, in measure_semantic_shift
+    
+    # Rename the column name of the metric we want to display on the graph for easier use
+    df.rename(columns={f'MEANING GAIN/LOSS {time_slice}':'Score'}, inplace=True)
+    
+    for i in range(nb_words):
+        # TODO need to have a nice cloud, so we need to modulate a bit the year
+        df['Year'] = year
+        # TODO rm 
+        df['Color'] = fill_color['gold']
+
+    return df 
+
+def results_all_periods(folder_path):
+    list_files = glob.glob(folder_path + "*.csv")
+    df1 = pd.read_csv('/home/mathilde/Documents/S9 Project/sandbox/src/ex_res_shift.csv')
+    df2 = pd.read_csv('/home/mathilde/Documents/S9 Project/sandbox/src/ex_res_shift.csv')
+    full_results = [df1, df2]
+    for f in list_files:
+        df = read_results_one_period(f)
+        full_results.append(df)
+    
+    df_full = pd.concat(full_results)
+
+    return df_full
