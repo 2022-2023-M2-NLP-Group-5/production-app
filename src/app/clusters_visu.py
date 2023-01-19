b09a9714d0d189350b478b3cfe3c6bfd7a0a829c
Mathilde Aguiar <mathilde.aguiar@etu.univ-grenoble-alpes.fr>
2023-01-19 13:12:06 +0100

    adjunction of the historical events on the cluster graph

--- src/app/clusters_visu.py
+++ src/app/clusters_visu.py
@@ -1,9 +1,10 @@
 from bokeh.plotting import figure, show
 from bokeh.embed import components
 from bokeh.models import (Arrow, ColumnDataSource, Label,
-                          NormalHead, SingleIntervalTicker, TapTool)
-from .visu_utils import results_all_periods
+                          NormalHead, SingleIntervalTicker, TapTool, Tooltip)
+from .visu_utils import results_all_periods  #, set_historical_envents
 import os
+import pandas as pd
 
 
 class Clusters_Visualization():
@@ -13,7 +14,7 @@ class Clusters_Visualization():
     #    self.target_word = target_word
 
     path = os.getcwd()
-    data = results_all_periods(path + "/", tg_word="awful")  # TODO fetch the tg_word from views 
+    data = results_all_periods(path + "/", tg_word="awful")  # TODO fetch the tg_word from views + TODO make a folder for the csv outputs
     source = ColumnDataSource(data) 
 
     # TODO change the caption of the third div
@@ -35,7 +36,7 @@ class Clusters_Visualization():
     plot.y_range.range_padding = 4
     plot.y_range.range_padding_units = "absolute"
 
-    plot.title.text = "Semantic shift of the word awful"  #+ self.target_word   # TODO make it dynamic
+    plot.title.text = "Semantic shift of the word awful"  #+ self.target_word   # TODO make it dynamic or just ditch it and use the h4 in the html 
     plot.title.text_font_size = "19px"
 
     plot.yaxis.ticker = SingleIntervalTicker(interval=10, num_minor_ticks=0)
@@ -44,12 +45,32 @@ class Clusters_Visualization():
     plot.ygrid.grid_line_dash = "dashed"
     plot.yaxis.axis_label = "Score"
 
-    plot.xaxis.ticker = [1880, 1900, 1920, 1940, 1960, 1980, 2000, 2020]
+    plot.xaxis.ticker = [1880, 1900, 1920, 1940, 1960, 1980, 2000, 2020] # TODO adapt it to the time slices 
     plot.xaxis.major_tick_in = -5
     plot.xaxis.major_tick_out = 10
     plot.xgrid.grid_line_color = None
     plot.xaxis.axis_label = "Year"
 
+    # TODO fix the pb when we slide to ticks that are not in the previous list 
+    plot.xgrid.band_hatch_pattern = "/"
+    plot.xgrid.band_hatch_alpha = 0.6
+    plot.xgrid.band_hatch_color = "lightgrey"
+    plot.xgrid.band_hatch_weight = 0.5
+    plot.xgrid.band_hatch_scale = 10
+
+    # TODO test for the axis of historical events
+
+    #hist_events = set_historical_envents()
+    df_hist = pd.read_csv("./historical_events_data/historical_events.csv")  # TODO en dur mais tant pis
+
+
+    for event in df_hist.values:
+        historical_event_label = Label(x=event[1], y=event[2], text=event[0],
+                            text_align="center", text_baseline="middle",
+                            text_font_size="12px", text_font_style="italic",
+                            text_color="lightsalmon")
+        plot.add_layout(historical_event_label)
+    
     medal = plot.circle(x="Year", y="Score", size='Size', level="overlay",  # size = 10
                         fill_color="Color", line_color="Color", fill_alpha=0.5, source=source)
     plot.hover.renderers = [medal]
@@ -57,3 +78,6 @@ class Clusters_Visualization():
     plot.text(x="Year", y="Score", x_offset=15, y_offset=-5,  
             text="word", text_align="left", text_baseline="middle",
             text_font_size="12px", text_color='TextColor', source=source)
+    
+ 
+
